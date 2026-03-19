ce549afd2ca82bc1ac1c5d67a5add53648e38cdd
KumarManuSaraswat <kumar.saraswat.s.125@kalvium.community>
2026-03-19 17:14:25 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+// Load env vars
+dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware to parse JSON bodies
+app.express.json();
+
+// Mount routes
+app.use('/api/users', require('./routes/userRoutes'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
